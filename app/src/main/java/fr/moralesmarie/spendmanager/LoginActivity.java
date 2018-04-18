f9bba4090e1f29b955a7a8930c0683578e28adcc
coulye <coolye@hotmail.fr>
2018-04-18 16:03:55 +0200

    MAJ
    choix via commentaire pour acces loca ou distant

--- app/src/main/java/fr/moralesmarie/spendmanager/LoginActivity.java
+++ app/src/main/java/fr/moralesmarie/spendmanager/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,10 +14,12 @@ import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
 
+import fr.moralesmarie.spendmanager.Class.Utilisateur;
+
 public class LoginActivity extends AppCompatActivity {
 
-    final String LOGIN_USER = "usr_login";
-    final String PASSWD_USER = "passwd_user";
+    final String LOGIN_USER = "user";
+    final String PASSWD_USER = "pass";
     EditText Identifiant, MDP;
 
     @Override
@@ -32,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
                 connexion();
             }
         });
+
     }
 
     public void connexion(){
@@ -40,12 +44,17 @@ public class LoginActivity extends AppCompatActivity {
         String loginSend = Identifiant.getText().toString();
         String passSend = MDP.getText().toString();
 
-//        String myUrl = "http://172.20.10.5/REST-API-SY4/public/login.php";
-        String myUrl = "http://127.0.0.1:8080/REST-API-SY4/public/login.php";
+        //accès en local
+//       String myUrl = "http://127.0.0.1:8080/REST-API-SY4/public/login.php";
+
+        //accès en ligne via le serveur moralesmarie.alwaysdata.net
+        String myUrl = "http://moralesmarie.alwaysdata.net/public/login.php";
         String params = "mail="+loginSend+"&mdp="+passSend;
 
         HttpPostRequest postRequest = new HttpPostRequest();
         try{
+            Log.i("test", "result "+result);
+
             result = postRequest.execute(new String []{myUrl, params}).get(); // exécution de la connexion
         }
         catch (InterruptedException e) {
@@ -53,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        System.out.println("Retour HTTPPostRequest : " + result + myUrl);
+        System.out.println("Retour HTTPPostRequest : " + result );
         try{
             JSONObject objLogin = new JSONObject(result);
             if (loginSend.equals(objLogin.getString("Mail_Utilisateur")) && passSend.equals(objLogin.get("Mdp_Utilisateur"))){
